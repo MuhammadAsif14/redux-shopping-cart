ace8e3264d2c7ad068811f44b5c8f0b384794200
MuhammadAsif14 <masif.bscsses21@iba-suk.edu.pk>
2025-03-16 16:02:01 +0500

    added CartItem and ProductCard componenets

---
+++ redux-shopping-cart/src/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>${product.price}</p>
+      <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
